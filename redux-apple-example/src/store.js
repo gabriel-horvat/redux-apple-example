cb1940317763f7771f0b0cfb76f872f37bff869d
Gabmeister <gabriel@gabrielhorvat.com>
2019-11-18 10:41:32 +0100

    initial redux setup

---
+++ redux-apple-example/src/store.js
@@ -0,0 +1,13 @@
+import { createStore, compose } from 'redux';
+import rootReducer from './reducers';
+
+const initialState = {};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeEnhancers
+  )
+
+export default store;
